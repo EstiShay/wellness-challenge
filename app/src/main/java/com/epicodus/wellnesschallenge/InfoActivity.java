b5fd5e709abc905ac3585f0c3f68880116b39c61
Esti Shay <esti.r.shay@gmail.com>
2017-11-06 16:21:53 -0800

    incorporate custom fonts

--- app/src/main/java/com/epicodus/wellnesschallenge/InfoActivity.java
+++ app/src/main/java/com/epicodus/wellnesschallenge/InfoActivity.java
@@ -1,16 +1,19 @@
 package com.epicodus.wellnesschallenge;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class InfoActivity extends AppCompatActivity implements View.OnClickListener{
-
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+    @Bind(R.id.aboutTextView) TextView mAboutTextView;
     @Bind(R.id.teamButton) Button mTeamButton;
     @Bind(R.id.dashButton) Button mDashButton;
 
@@ -20,6 +23,10 @@ public class InfoActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_info);
         ButterKnife.bind(this);
 
+        Typeface ranchoFont = Typeface.createFromAsset(getAssets(), "fonts/rancho.ttf");
+        mAppNameTextView.setTypeface(ranchoFont);
+        Typeface commeFont = Typeface.createFromAsset(getAssets(), "fonts/commeregular.ttf");
+        mAboutTextView.setTypeface(commeFont);
 
         mTeamButton.setOnClickListener(this);
         mDashButton.setOnClickListener(this);
@@ -30,6 +37,8 @@ public class InfoActivity extends AppCompatActivity implements View.OnClickListe
         if (v == mDashButton) {
             Intent intent = new Intent(InfoActivity.this, MainActivity.class);
             startActivity(intent);
+        } else if (v == mTeamButton) {
+
         }
     }
 }
--- app/src/main/java/com/epicodus/wellnesschallenge/MainActivity.java
+++ app/src/main/java/com/epicodus/wellnesschallenge/MainActivity.java
@@ -1,16 +1,19 @@
 package com.epicodus.wellnesschallenge;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+    @Bind(R.id.subtitleTextView) TextView mSubtitleTextView;
     @Bind(R.id.addButton) Button mAddButton;
     @Bind(R.id.myLogButton) Button mMyLogButton;
     @Bind(R.id.teamButton) Button mTeamButton;
@@ -21,6 +24,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        Typeface ranchoFont = Typeface.createFromAsset(getAssets(), "fonts/rancho.ttf");
+        mAppNameTextView.setTypeface(ranchoFont);
+        Typeface commeFont = Typeface.createFromAsset(getAssets(), "fonts/commeregular.ttf");
+        mSubtitleTextView.setTypeface(commeFont);
 
         mAddButton.setOnClickListener(this);
         mMyLogButton.setOnClickListener(this);
@@ -33,6 +41,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (v == mInfoButton) {
             Intent intent = new Intent(MainActivity.this, InfoActivity.class);
             startActivity(intent);
+        } else if (v == mMyLogButton) {
+
+        } else if (v == mTeamButton) {
+
+        } else if (v == mAddButton) {
+
         }
     }
 }
