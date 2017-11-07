920725de2c03f6aeeb5b6503cde743ca1e2609a4
Esti Shay <esti.r.shay@gmail.com>
2017-11-07 13:22:37 -0800

    create UserLogActivity

--- app/src/main/java/com/epicodus/wellnesschallenge/ui/UserLogActivity.java
+++ app/src/main/java/com/epicodus/wellnesschallenge/ui/UserLogActivity.java
@@ -1,19 +1,41 @@
 package com.epicodus.wellnesschallenge.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.epicodus.wellnesschallenge.MainActivity;
 import com.epicodus.wellnesschallenge.R;
 
-public class UserLogActivity extends AppCompatActivity {
-//    private DatabaseReference mTrackReference;
-//    private FirebaseExerciseListAdapter mFirebaseAdapter;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class UserLogActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.dashButton) Button mDashButton;
+    @Bind(R.id.infoButton) Button mInfoButton;
+    @Bind(R.id.teamButton) Button mTeamButton;
+    @Bind(R.id.addButton) Button mAddButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_log);
+        ButterKnife.bind(this);
 
-//        setUpFirebaseAdapter();
+        mDashButton.setOnClickListener(this);
+        mInfoButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mInfoButton) {
+            Intent intent = new Intent(UserLogActivity.this, InfoActivity.class);
+            startActivity(intent);
+        } else if (v == mDashButton) {
+            Intent intent = new Intent(UserLogActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
