319f410fba4f191cd75d9071bbb98acb73dd749a
Esti Shay <esti.r.shay@gmail.com>
2017-11-07 12:00:48 -0800

    creat exercise model

---
+++ app/src/main/java/com/epicodus/wellnesschallenge/models/Exercise.java
@@ -0,0 +1,28 @@
+package com.epicodus.wellnesschallenge.models;
+
+import org.parceler.Parcel;
+
+@Parcel
+public class Exercise {
+    String date;
+    String exerciseType;
+    double miles;
+
+    public Exercise(){}
+
+    public Exercise(String date, String exerciseType, double miles){
+        this.date = date;
+        this.exerciseType = exerciseType;
+        this.miles = miles;
+    }
+
+    public String getDate(){
+        return date;
+    }
+    public String getExerciseType(){
+        return exerciseType;
+    }
+    public double getMiles(){
+        return miles;
+    }
+}
