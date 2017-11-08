fc6d4e4b78252eb24bc2a57b33d1bc58ef6b9cbe
Esti Shay <esti.r.shay@gmail.com>
2017-11-08 10:00:02 -0800

    add create account activity

---
+++ app/src/main/java/com/epicodus/wellnesschallenge/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.wellnesschallenge.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.wellnesschallenge.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
