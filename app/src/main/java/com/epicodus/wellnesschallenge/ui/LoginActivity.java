6ee90efb2ee82d8db94984e87ee833e038914237
Esti Shay <esti.r.shay@gmail.com>
2017-11-08 10:06:57 -0800

    add login activity

---
+++ app/src/main/java/com/epicodus/wellnesschallenge/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.wellnesschallenge.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.wellnesschallenge.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
